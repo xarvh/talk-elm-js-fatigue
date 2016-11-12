ac4443019f7923cb4ff0ee5ec697b0f2a4227c36
xarvh <francesco.orsenigo@gmail.com>
2016-11-12 19:27:00 +1100

    Consolidate general layout

--- Talk.elm
+++ Talk.elm
@@ -5,75 +5,70 @@ import Slides exposing (..)
 main = app
     slidesDefaultOptions
 
-    [ md
-        """
-        TODO: Title
-        """
+    [ md "Elm as an antidote to front-end fatigue"
+
+    , mdFragments
+        [ "**Elm**"
+        , "- Guarantees no runtime errors"
+        , "- Time-travelling debugger"
+        , "- blah blah blah"
+        , "- blah"
+        , "- yet the *latest* over-hyped tech"
+        , " ... "
+        ]
+
+    , mdFragments
+        [ "The truth is that learning the language was not easy"
+        , "(OOP patterns aren't really useful in FP)"
+        , "(Also I'm lazy)"
+        ]
+
+    , mdFragments
+        [ "So why stick with Elm?"
+        , "**Because front-end JavaScript sucks**"
+        ]
+
+    , mdFragments
+        [
+            -- show tutorial
+            --         """
+            --         JS Fatigue
+            -- 
+            --         or: why I need a 10 page tutorial before I can start actually writing my code
+            --         """
+        ]
+
+    , mdFragments
+        [ "JavaScript definitions:"
+        , "1. The only language that runs on all browsers"
+        , "2. A jumble of stuff we use to build code that runs on all browsers"
+        ]
 
     , md
         """
-        * Guarantees no runtime errors
-        * Time-travelling debugger
-        * Blah
-        * Blah
-        * ....
+        let's rethink stuff
+        we are compiling anyway
         """
-
-
-    , md
-        """
-        The truth is that I struggled to learn the language
-
-        (OOP patterns are kind of useless in FP)
-        """
-
-
-    , md
-        """
-        So what made me stick with it?
-
-        -> "simpler build pipeline"?
-        """
-
-
-    , md
-        """
-        Writing Elm was on par with writing Python or JS for Node, nothing exceptional
-
-        Shifting view
-
-            * Elm is not exceptional, just normal
-            * It's front end JS that sucks, and there is a name for it
-        """
-
-
-    , md
-        """
-        JS Fatigue
-
-        or: why I need a 10 page tutorial before I can start actually writing my code
-        """
-
-
-
-    , md
-        """
-        Let's step back, reconsider how we are doing things
-
-        """
-
 
     , md
         """
         This is what happened with Elm
 
         -> show minimal program
+
+        -> list all build passages that in JS we'd have to do:
+            - dependency installation
+            - syntax check (usually via linting)
+            - type check
+            - packaging
+            - translation from JSX/ES6/Flow
+            - bundling
+            - (all while trying not to recompile stuff that hasn't changed)
         """
-
-
 
     , md
         """
-        Advantage of having everything 
+        Integrated type safety
+        -> example with no type declarations
         """
     ]
